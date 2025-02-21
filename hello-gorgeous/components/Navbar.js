4742f3da1e016d23d1104064321bac48096da05b
Finley Mwangola <finley.mwachia12@gmail.com>
2025-02-21 12:06:19 +0300

    make changes to the Navbar js file

--- hello-gorgeous/components/Navbar.js
+++ hello-gorgeous/components/Navbar.js
@@ -4,18 +4,18 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-md p-4">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <Link href="/">
-          <h1 className="text-2xl font-bold text-pink-600 cursor-pointer">Hello Gorgeous</h1>
+        <Link href="/" className="text-2xl font-bold text-pink-600">
+          Hello Gorgeous
         </Link>
         <div>
-          <Link href="/shop">
-            <a className="mx-4 text-gray-700 hover:text-pink-600">Shop</a>
+          <Link href="/shop" className="mx-4 text-gray-700 hover:text-pink-600">
+            Shop
           </Link>
-          <Link href="/booking">
-            <a className="mx-4 text-gray-700 hover:text-pink-600">Book Makeup</a>
+          <Link href="/booking" className="mx-4 text-gray-700 hover:text-pink-600">
+            Book Makeup
           </Link>
-          <Link href="/account">
-            <a className="mx-4 text-gray-700 hover:text-pink-600">Account</a>
+          <Link href="/account" className="mx-4 text-gray-700 hover:text-pink-600">
+            Account
           </Link>
         </div>
       </div>
